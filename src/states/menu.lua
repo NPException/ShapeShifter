21399e2fdc47803a8ae53613f2bcd62e433c435f
ChaosChaot <git@chaoschaot.de>
2016-04-16 17:09:57 +0200

    add start-button, change position and color

--- src/states/menu.lua
+++ src/states/menu.lua
@@ -13,14 +13,15 @@ function Menu:draw()
   
   lg.draw(images.backgrounds.background_ph,0,0)
     
-  local width = lg.getFont():getWidth("Shift It")
+  local width = lg.getFont():getWidth("ShapeShifter")
   local height = lg.getFont():getHeight()
   
   
-  lg.setColor(255,255,255,170)
+  lg.setColor(255,255,255)
   
-  lg.print("Shift It",100,100,0,2,2, width/2, height/2)
+  lg.print("ShapeShifter",150,100,0,2,2, width/2, height/2)
   
+  lg.print("-> Start <-",150,500,0,2,2, width/2, height/2)
 end
 
 return Menu
