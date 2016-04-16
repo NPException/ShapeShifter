8ed90cbd2aa2c253335e3cae90ffd1c2bb9e9079
NPException <npexception42@gmail.com>
2016-04-16 17:06:20 +0200

    improved fader

--- src/states/fader.lua
+++ src/states/fader.lua
@@ -1,6 +1,7 @@
 local Fader = {}
 Fader.__index = function (table, key)
   local faderMethod = Fader[key]
+  -- if there is a method explicitly declared in Fader we return it, otherwise we just hand out the wrapped state's method
   return faderMethod and faderMethod or table.state[key]
 end
 
@@ -9,11 +10,15 @@ local setColor = love.graphics.setColor
 local fill = love.graphics.rectangle
 local floor = math.floor
 
-
-function Fader.fader( state, fadeIn, duration, doneCallback )
+--[[
+  Creates a new fader state, that will fade from/to the given colors with in the given amount of time.
+  After the fade is complete, it will execute the doneCallback function.
+]]--
+function Fader.fader( state, fadeIn, duration, doneCallback, colors )
   local wrap = setmetatable({}, Fader)
   wrap.state = state
   wrap.fadeIn = fadeIn
+  wrap.colors = colors or {0,0,0}
   wrap.alpha = fadeIn and 255 or 0
   wrap.duration = duration
   wrap.doneCallback = doneCallback
@@ -21,33 +26,45 @@ function Fader.fader( state, fadeIn, duration, doneCallback )
 end
 
 function Fader:update( dt )
+  -- update the wrapped state if it has an update method
   local stateUpdate = self.state.update
   if stateUpdate then
     stateUpdate(dt)
   end
   
+  -- don't update if the duration was set to 0
   if (self.duration == 0) then
     return
   end
   
+  -- calculate new alpha value based on dt
   local factor = self.fadeIn and -1 or 1
   local diff = (255.0/self.duration) * factor * dt
-  self.alpha = self.alpha + diff
+  local alpha = self.alpha + diff
   
-  if (self.alpha < 0 or self.alpha > 255) then
-    self.alpha = self.alpha < 0 and 0 or self.alpha > 255 and 255 or self.alpha
+  -- if the alpha reached a limit
+  if (alpha < 0 or alpha > 255) then
+    -- clamp it to the range 0-255
+    alpha = alpha < 0 and 0 or alpha > 255 and 255 or alpha
+    -- execute the doneCallback if present
     if (self.doneCallback) then
       self.doneCallback()
     end
+    -- deactivate the fader
     self.duration = 0
   end
+  -- set actuall alpha value, and the value used in draw()
+  self.alpha = alpha
+  self.colors[4] = floor(alpha +0.5)
 end
 
 function Fader:draw()
+  -- draw the state first
   self.state:draw()
   
+  -- cover it with our fader color
   local width, height = love.graphics.getDimensions()
-  setColor(0,0,0,floor(self.alpha+0.5))
+  setColor(self.colors)
   fill("fill", 0,0,width, height)
 end
 
